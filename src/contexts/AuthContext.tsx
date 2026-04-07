17fccff4f98bf676e504aee339f11fabd5b25c35
Rafael Queiroz <rafaelproinfo@gmail.com>
2026-04-07 16:30:45 -0300

    ✨ New Feature: Consumo do Recurso Tema

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -2,7 +2,8 @@ import { createContext, useState, type ReactNode } from "react";
 import type UsuarioLogin from "../models/UsuarioLogin";
 import { login } from "../services/Service";
 
-
+// Todos os estados e funções que serão compartilhadas
+// com toda a minha aplicação
 interface AuthContextProps{
   usuario: UsuarioLogin
   handleLogout(): void
@@ -10,12 +11,16 @@ interface AuthContextProps{
   isLoading: boolean
 }
 
+// Quem irá consumir o meu provedor
 interface AuthProviderProps{
   children: ReactNode
 }
 
+// Criar o meu contexto com a tipagem AuthContextProps
+// O meu contexto irá disponibilizar os estados e funções do tipo AuthContextProps
 export const AuthContext = createContext({} as AuthContextProps)
 
+// Inicializar os estados e implementar as funções dentro do provedor
 export function AuthProvider({ children }: AuthProviderProps){
 
   // Inicializar o estado usuario (armazenar os dados do usuário autenticado)
